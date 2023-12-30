18040800f83ff0b9ecf83fc15b9500ccf658a45d
Filipa Lovrić <115006963+filipa131@users.noreply.github.com>
2023-12-30 16:37:57 +0100

    Napravljen izgled Forma1 i Forma4

---
+++ PoliceStation/PoliceStation/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace PoliceStation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.editEmployee = new System.Windows.Forms.Button();
+            this.addEmployee = new System.Windows.Forms.Button();
+            this.idAdd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.hourlyRate = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.dateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.departmentAdd = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.filterEmployee = new System.Windows.Forms.Button();
+            this.idFilter = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.departmentFilter = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.deleteEmployee = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // editEmployee
+            // 
+            this.editEmployee.AutoSize = true;
+            this.editEmployee.Location = new System.Drawing.Point(133, 507);
+            this.editEmployee.Name = "editEmployee";
+            this.editEmployee.Size = new System.Drawing.Size(75, 30);
+            this.editEmployee.TabIndex = 26;
+            this.editEmployee.Text = "Edit";
+            this.editEmployee.UseVisualStyleBackColor = true;
+            // 
+            // addEmployee
+            // 
+            this.addEmployee.AutoSize = true;
+            this.addEmployee.Location = new System.Drawing.Point(31, 507);
+            this.addEmployee.Name = "addEmployee";
+            this.addEmployee.Size = new System.Drawing.Size(75, 30);
+            this.addEmployee.TabIndex = 25;
+            this.addEmployee.Text = "Add";
+            this.addEmployee.UseVisualStyleBackColor = true;
+            // 
+            // idAdd
+            // 
+            this.idAdd.Location = new System.Drawing.Point(34, 191);
+            this.idAdd.Name = "idAdd";
+            this.idAdd.Size = new System.Drawing.Size(164, 26);
+            this.idAdd.TabIndex = 24;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 20);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "ID";
+            // 
+            // hourlyRate
+            // 
+            this.hourlyRate.Location = new System.Drawing.Point(32, 454);
+            this.hourlyRate.Name = "hourlyRate";
+            this.hourlyRate.Size = new System.Drawing.Size(164, 26);
+            this.hourlyRate.TabIndex = 22;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(30, 102);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(164, 26);
+            this.name.TabIndex = 21;
+            // 
+            // dateOfBirth
+            // 
+            this.dateOfBirth.Location = new System.Drawing.Point(33, 360);
+            this.dateOfBirth.Name = "dateOfBirth";
+            this.dateOfBirth.Size = new System.Drawing.Size(200, 26);
+            this.dateOfBirth.TabIndex = 20;
+            // 
+            // departmentAdd
+            // 
+            this.departmentAdd.FormattingEnabled = true;
+            this.departmentAdd.Items.AddRange(new object[] {
+            "Patrol Division",
+            "Investigations Division",
+            "Special Operations",
+            "Highway Patrol",
+            "Community Services",
+            "Crime Scene Units",
+            "Administrative Division",
+            "Internal Affairs"});
+            this.departmentAdd.Location = new System.Drawing.Point(31, 276);
+            this.departmentAdd.Name = "departmentAdd";
+            this.departmentAdd.Size = new System.Drawing.Size(164, 28);
+            this.departmentAdd.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 418);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Hourly Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Date of Birth";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Department";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Name";
+            // 
+            // filterEmployee
+            // 
+            this.filterEmployee.AutoSize = true;
+            this.filterEmployee.Location = new System.Drawing.Point(76, 801);
+            this.filterEmployee.Name = "filterEmployee";
+            this.filterEmployee.Size = new System.Drawing.Size(75, 30);
+            this.filterEmployee.TabIndex = 31;
+            this.filterEmployee.Text = "Filter";
+            this.filterEmployee.UseVisualStyleBackColor = true;
+            // 
+            // idFilter
+            // 
+            this.idFilter.Location = new System.Drawing.Point(35, 665);
+            this.idFilter.Name = "idFilter";
+            this.idFilter.Size = new System.Drawing.Size(164, 26);
+            this.idFilter.TabIndex = 30;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 628);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 20);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Filter by ID";
+            // 
+            // departmentFilter
+            // 
+            this.departmentFilter.FormattingEnabled = true;
+            this.departmentFilter.Items.AddRange(new object[] {
+            "Patrol Division",
+            "Investigations Division",
+            "Special Operations",
+            "Highway Patrol",
+            "Community Services",
+            "Crime Scene Units",
+            "Administrative Division",
+            "Internal Affairs"});
+            this.departmentFilter.Location = new System.Drawing.Point(32, 750);
+            this.departmentFilter.Name = "departmentFilter";
+            this.departmentFilter.Size = new System.Drawing.Size(164, 28);
+            this.departmentFilter.TabIndex = 28;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(28, 717);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(153, 20);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "Filter by Department";
+            // 
+            // deleteEmployee
+            // 
+            this.deleteEmployee.AutoSize = true;
+            this.deleteEmployee.Location = new System.Drawing.Point(76, 558);
+            this.deleteEmployee.Name = "deleteEmployee";
+            this.deleteEmployee.Size = new System.Drawing.Size(75, 30);
+            this.deleteEmployee.TabIndex = 34;
+            this.deleteEmployee.Text = "Delete";
+            this.deleteEmployee.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(289, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(844, 676);
+            this.dataGridView1.TabIndex = 35;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 850);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.deleteEmployee);
+            this.Controls.Add(this.filterEmployee);
+            this.Controls.Add(this.idFilter);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.departmentFilter);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.editEmployee);
+            this.Controls.Add(this.addEmployee);
+            this.Controls.Add(this.idAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.hourlyRate);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.dateOfBirth);
+            this.Controls.Add(this.departmentAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Employees";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button editEmployee;
+        private System.Windows.Forms.Button addEmployee;
+        private System.Windows.Forms.TextBox idAdd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox hourlyRate;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.DateTimePicker dateOfBirth;
+        private System.Windows.Forms.ComboBox departmentAdd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button filterEmployee;
+        private System.Windows.Forms.TextBox idFilter;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox departmentFilter;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button deleteEmployee;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ PoliceStation/PoliceStation/Form4.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace PoliceStation
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TextBox policeInventory;
+            this.filterInventory = new System.Windows.Forms.Button();
+            this.sNoFilter = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.inventoryTypeFilter = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.editInventory = new System.Windows.Forms.Button();
+            this.addInventory = new System.Windows.Forms.Button();
+            this.sNoAdd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.quantityAdd = new System.Windows.Forms.TextBox();
+            this.departmentAdd = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.deleteInventory = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            policeInventory = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // filterInventory
+            // 
+            this.filterInventory.AutoSize = true;
+            this.filterInventory.Location = new System.Drawing.Point(78, 811);
+            this.filterInventory.Name = "filterInventory";
+            this.filterInventory.Size = new System.Drawing.Size(75, 30);
+            this.filterInventory.TabIndex = 54;
+            this.filterInventory.Text = "Filter";
+            this.filterInventory.UseVisualStyleBackColor = true;
+            // 
+            // sNoFilter
+            // 
+            this.sNoFilter.Location = new System.Drawing.Point(37, 675);
+            this.sNoFilter.Name = "sNoFilter";
+            this.sNoFilter.Size = new System.Drawing.Size(164, 26);
+            this.sNoFilter.TabIndex = 53;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 638);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 20);
+            this.label7.TabIndex = 52;
+            this.label7.Text = "Filter by S No";
+            // 
+            // inventoryTypeFilter
+            // 
+            this.inventoryTypeFilter.FormattingEnabled = true;
+            this.inventoryTypeFilter.Items.AddRange(new object[] {
+            "Patrol Division",
+            "Investigations Division",
+            "Special Operations",
+            "Highway Patrol",
+            "Community Services",
+            "Crime Scene Units",
+            "Administrative Division",
+            "Internal Affairs"});
+            this.inventoryTypeFilter.Location = new System.Drawing.Point(34, 760);
+            this.inventoryTypeFilter.Name = "inventoryTypeFilter";
+            this.inventoryTypeFilter.Size = new System.Drawing.Size(164, 28);
+            this.inventoryTypeFilter.TabIndex = 51;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 727);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(171, 20);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Filter by Inventory Type";
+            // 
+            // editInventory
+            // 
+            this.editInventory.AutoSize = true;
+            this.editInventory.Location = new System.Drawing.Point(124, 447);
+            this.editInventory.Name = "editInventory";
+            this.editInventory.Size = new System.Drawing.Size(75, 30);
+            this.editInventory.TabIndex = 49;
+            this.editInventory.Text = "Edit";
+            this.editInventory.UseVisualStyleBackColor = true;
+            // 
+            // addInventory
+            // 
+            this.addInventory.AutoSize = true;
+            this.addInventory.Location = new System.Drawing.Point(22, 447);
+            this.addInventory.Name = "addInventory";
+            this.addInventory.Size = new System.Drawing.Size(75, 30);
+            this.addInventory.TabIndex = 48;
+            this.addInventory.Text = "Add";
+            this.addInventory.UseVisualStyleBackColor = true;
+            // 
+            // sNoAdd
+            // 
+            this.sNoAdd.Location = new System.Drawing.Point(29, 212);
+            this.sNoAdd.Name = "sNoAdd";
+            this.sNoAdd.Size = new System.Drawing.Size(164, 26);
+            this.sNoAdd.TabIndex = 47;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 20);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "S No";
+            // 
+            // quantityAdd
+            // 
+            this.quantityAdd.Location = new System.Drawing.Point(23, 394);
+            this.quantityAdd.Name = "quantityAdd";
+            this.quantityAdd.Size = new System.Drawing.Size(164, 26);
+            this.quantityAdd.TabIndex = 45;
+            // 
+            // policeInventory
+            // 
+            policeInventory.Location = new System.Drawing.Point(23, 123);
+            policeInventory.Name = "policeInventory";
+            policeInventory.Size = new System.Drawing.Size(164, 26);
+            policeInventory.TabIndex = 44;
+            // 
+            // departmentAdd
+            // 
+            this.departmentAdd.FormattingEnabled = true;
+            this.departmentAdd.Items.AddRange(new object[] {
+            "Weapons and Ammunition",
+            "Communication Devices",
+            "Protective Gear",
+            "Vehicles",
+            "Technology and Tools",
+            "Office Supplies",
+            "Training Equipment",
+            "Medical Supplies",
+            "Miscellaneous"});
+            this.departmentAdd.Location = new System.Drawing.Point(26, 297);
+            this.departmentAdd.Name = "departmentAdd";
+            this.departmentAdd.Size = new System.Drawing.Size(164, 28);
+            this.departmentAdd.TabIndex = 43;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Quantity";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Inventory Type";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "Police Inventory";
+            // 
+            // deleteInventory
+            // 
+            this.deleteInventory.AutoSize = true;
+            this.deleteInventory.Location = new System.Drawing.Point(67, 500);
+            this.deleteInventory.Name = "deleteInventory";
+            this.deleteInventory.Size = new System.Drawing.Size(75, 30);
+            this.deleteInventory.TabIndex = 55;
+            this.deleteInventory.Text = "Delete";
+            this.deleteInventory.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(249, 123);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(851, 665);
+            this.dataGridView1.TabIndex = 56;
+            // 
+            // Inventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 875);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.deleteInventory);
+            this.Controls.Add(this.filterInventory);
+            this.Controls.Add(this.sNoFilter);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.inventoryTypeFilter);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.editInventory);
+            this.Controls.Add(this.addInventory);
+            this.Controls.Add(this.sNoAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.quantityAdd);
+            this.Controls.Add(policeInventory);
+            this.Controls.Add(this.departmentAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Inventory";
+            this.Text = "Inventory";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button filterInventory;
+        private System.Windows.Forms.TextBox sNoFilter;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox inventoryTypeFilter;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button editInventory;
+        private System.Windows.Forms.Button addInventory;
+        private System.Windows.Forms.TextBox sNoAdd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox quantityAdd;
+        private System.Windows.Forms.ComboBox departmentAdd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button deleteInventory;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
