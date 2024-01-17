1784f6b6f155e7575918122be12a01ea56c076d5
Filipa Lovrić <115006963+filipa131@users.noreply.github.com>
2024-01-17 23:34:08 +0100

    Fix LogIn

---
+++ RP3/PoliceStation/PoliceStation/Form3.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PoliceStation
+{
+    public partial class Form3 : Form
+    {
+        public Form3(bool writeAccess)
+        {
+            InitializeComponent();
+            typeOfCase.Enabled = writeAccess;
+            status.Enabled = writeAccess;
+            dateShift.Enabled = writeAccess;
+            addShift.Enabled = writeAccess;
+            saveShift.Enabled = writeAccess;
+            deleteShift.Enabled = writeAccess;
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'policeStationDataSet2.CasesTable' table. You can move, or remove it, as needed.
+            this.casesTableTableAdapter.Fill(this.policeStationDataSet2.CasesTable);
+
+        }
+
+        // ADD, SAVE, DELETE:
+        private void addShift_Click(object sender, EventArgs e)
+        {
+            casesTableBindingSource.AddNew();
+        }
+
+        private void saveShift_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                casesTableBindingSource.EndEdit();
+                casesTableTableAdapter.Update(policeStationDataSet2.CasesTable);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void deleteShift_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                casesTableBindingSource.RemoveCurrent();
+                casesTableTableAdapter.Update(policeStationDataSet2.CasesTable);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // PRINT:
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap imagebmp = new Bitmap(dataGridView1.Width, dataGridView1.Height);
+            dataGridView1.DrawToBitmap(imagebmp, new Rectangle(0, 0, dataGridView1.Width, dataGridView1.Height));
+            e.Graphics.DrawImage(imagebmp, 120, 20);
+        }
+
+        private void printCases_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.PrintPreviewControl.Zoom = 1;
+            printPreviewDialog1.ShowDialog();
+        }
+
+        // SORT:
+        private void sortCases_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataView dv = policeStationDataSet2.CasesTable.DefaultView;
+            dv.Sort = sortCases.SelectedItem.ToString();
+            dataGridView1.DataSource = dv.ToTable();
+        }
+
+        // FILTERS:
+        private void filterCases_Click(object sender, EventArgs e)
+        {
+            DataView dv = policeStationDataSet2.CasesTable.DefaultView;
+            string filterText = noFilter.Text.Trim();
+
+            if (!string.IsNullOrEmpty(filterText))
+            {
+                dv.RowFilter = $"Convert(No, 'System.String') = '{filterText}'";
+            }
+            else
+            {
+                dv.RowFilter = string.Empty;
+            }
+
+            dataGridView1.DataSource = dv.ToTable();
+        }
+
+        private void typeOfCaseFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataView dv = policeStationDataSet2.CasesTable.DefaultView;
+            string selectedValue = typeOfCaseFilter.SelectedItem?.ToString();
+
+            if (!string.IsNullOrEmpty(selectedValue))
+            {
+                dv.RowFilter = $"[Type of Case] = '{selectedValue}'";
+            }
+            else
+            {
+                dv.RowFilter = string.Empty;
+            }
+
+            dataGridView1.DataSource = dv.ToTable();
+        }
+
+        private void statusFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataView dv = policeStationDataSet2.CasesTable.DefaultView;
+            string selectedValue = statusFilter.SelectedItem?.ToString();
+
+            if (!string.IsNullOrEmpty(selectedValue))
+            {
+                dv.RowFilter = $"Status = '{selectedValue}'";
+            }
+            else
+            {
+                dv.RowFilter = string.Empty;
+            }
+
+            dataGridView1.DataSource = dv.ToTable();
+        }
+
+        private void removeFilterCases_Click(object sender, EventArgs e)
+        {
+            DataView dv = policeStationDataSet2.CasesTable.DefaultView;
+            dv.RowFilter = string.Empty;
+
+            dataGridView1.DataSource = dv.ToTable();
+        }
+    }
+}
