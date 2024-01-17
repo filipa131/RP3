4ed48dd8119e77595370f06ac4ee892a8ceea875
Filipa Lovrić <115006963+filipa131@users.noreply.github.com>
2024-01-17 23:38:24 +0100

    fix login

---
+++ PoliceStation/Form4.cs
@@ -0,0 +1,132 @@
+﻿using PoliceStation.PoliceStationDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PoliceStation
+{
+    public partial class Form4 : Form
+    {
+        public Form4(bool writeAccess)
+        {
+            InitializeComponent();
+            sNoAdd.Enabled = writeAccess;
+            departmentAdd.Enabled = writeAccess;
+            quantityAdd.Enabled = writeAccess;
+            addInventory.Enabled = writeAccess;
+            saveInventory.Enabled = writeAccess;
+            deleteInventory.Enabled = writeAccess;
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'policeStationDataSet.InventoryTable' table. You can move, or remove it, as needed.
+            this.inventoryTableTableAdapter.Fill(this.policeStationDataSet.InventoryTable);
+
+        }
+
+        // ADD, SAVE, DELETE:
+        private void addInventory_Click(object sender, EventArgs e)
+        {
+            inventoryTableBindingSource.AddNew();
+        }
+
+        private void saveInventory_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                inventoryTableBindingSource.EndEdit();
+                inventoryTableTableAdapter.Update(policeStationDataSet.InventoryTable);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void deleteInventory_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                inventoryTableBindingSource.RemoveCurrent();
+                inventoryTableTableAdapter.Update(policeStationDataSet.InventoryTable);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // PRINT:
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap imagebmp = new Bitmap(dataGridView1.Width, dataGridView1.Height);
+            dataGridView1.DrawToBitmap(imagebmp, new Rectangle(0, 0, dataGridView1.Width, dataGridView1.Height));
+            e.Graphics.DrawImage(imagebmp, 120, 20);
+        }
+
+        private void printInventory_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.PrintPreviewControl.Zoom = 1;
+            printPreviewDialog1.ShowDialog();
+        }
+
+        // SORT:
+        private void sortInventory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataView dv = policeStationDataSet.InventoryTable.DefaultView;
+            dv.Sort = sortInventory.SelectedItem.ToString();
+            dataGridView1.DataSource = dv.ToTable();
+        }
+
+        // FILTER:
+        private void filterInventory_Click(object sender, EventArgs e)
+        {
+            DataView dv = policeStationDataSet.InventoryTable.DefaultView;
+            string filterText = sNoFilter.Text.Trim();
+
+            if (!string.IsNullOrEmpty(filterText))
+            {
+                dv.RowFilter = $"Convert([S No], 'System.String') = '{filterText}'";
+            }
+            else
+            {
+                dv.RowFilter = string.Empty;
+            }
+
+            dataGridView1.DataSource = dv.ToTable();
+        }
+
+        private void inventoryTypeFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataView dv = policeStationDataSet.InventoryTable.DefaultView;
+            string selectedValue = inventoryTypeFilter.SelectedItem?.ToString();
+
+            if (!string.IsNullOrEmpty(selectedValue))
+            {
+                dv.RowFilter = $"[Inventory Type] = '{selectedValue}'";
+            }
+            else
+            {
+                dv.RowFilter = string.Empty;
+            }
+
+            dataGridView1.DataSource = dv.ToTable();
+        }
+
+        private void removeFilterInventory_Click(object sender, EventArgs e)
+        {
+            DataView dv = policeStationDataSet.InventoryTable.DefaultView;
+            dv.RowFilter = string.Empty;
+
+            dataGridView1.DataSource = dv.ToTable();
+        }
+    }
+}
---
+++ PoliceStation/MainForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PoliceStation
+{
+    public partial class MainForm : Form
+    {
+        private Form activeForm;
+        private bool loggedIn;
+        private bool writeAccess;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenForm(Form form)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+
+            activeForm = form;
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Dock = DockStyle.Fill;
+            Controls.Add(form);
+            form.Show();
+        }
+
+        private void employeesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenForm(new Form1(writeAccess));
+        }
+
+        private void shiftsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenForm(new Form2(writeAccess));
+        }
+
+        private void casesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenForm(new Form3(writeAccess));
+        }
+
+        private void inventoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenForm(new Form4(writeAccess));
+        }
+
+        private void logOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            writeAccess = false;
+            loggedIn = false;
+
+            textBoxID.Enabled = true;
+            textBoxPassword.Enabled = true;
+
+            employeesToolStripMenuItem.Enabled = false;
+            shiftsToolStripMenuItem.Enabled = false;
+            casesToolStripMenuItem.Enabled = false;
+            inventoryToolStripMenuItem.Enabled = false;
+            logOutToolStripMenuItem.Enabled = false;
+
+            labelMsg.Text = "Enter employee ID and password to log in.";
+            labelName.Text = "";
+            activeForm.Close();
+        }
+
+        string connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=PoliceStation;Integrated Security=True";
+
+        private void buttonLogIn_Click(object sender, EventArgs e)
+        {
+            // TODO: provjera podataka u bazi
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Password, [Write Access], Name" +
+                    " FROM [PoliceStation].[dbo].[EmployeesTable] WHERE ID=@ID", conn);
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("ID", textBoxID.Text);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read() && reader["Password"].ToString() == textBoxPassword.Text) 
+                {
+                    loggedIn = true;
+                    writeAccess = Convert.ToBoolean(reader["Write Access"]);
+                    labelName.Text = reader["Name"].ToString();
+                }
+            }
+
+            if (loggedIn)
+            {
+                textBoxID.Enabled = false;
+                textBoxPassword.Enabled = false;
+
+                employeesToolStripMenuItem.Enabled = true;
+                shiftsToolStripMenuItem.Enabled = true;
+                casesToolStripMenuItem.Enabled = true;
+                inventoryToolStripMenuItem.Enabled = true;
+                logOutToolStripMenuItem.Enabled = true;
+
+                labelMsg.Text = "You have successfully logged in. Welcome!";
+            }
+            else
+            {
+                labelMsg.Text = "Incorrect employee ID or password. Please try again!";
+            }
+        }
+    }
+}
